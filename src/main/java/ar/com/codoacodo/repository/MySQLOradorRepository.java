0200d45f5d661a4b4cbb0599e199e0d560a3516f
Bianca Stork <biancastrk@gmail.com>
2023-12-07 23:53:20 -0300

    agregue metodos del CRUD

--- src/main/java/ar/com/codoacodo/repository/MySQLOradorRepository.java
+++ src/main/java/ar/com/codoacodo/repository/MySQLOradorRepository.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.util.ArrayList;
 //import java.util.ArrayList;
 import java.util.List;
 
@@ -70,20 +71,64 @@ public class MySQLOradorRepository implements OradorRepository{
 
 
 	public void update(Orador orador) {
-		// TODO Auto-generated method stub
+		String sql = "update orador "
+				+ "set nombre=?, apellido=?, tema=?"
+				+ "where id = ?";
+		
+		try(Connection conn = AdministradorDeConexiones.getConnection()) {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			
+			statement.setString(1, orador.getNombre());
+			statement.setString(2, orador.getApellido());
+			//statement.setString(3, orador.getMail());
+			statement.setString(3, orador.getTema());
+			statement.setLong(4, orador.getId());
+			
+			statement.executeUpdate();
+		}catch(Exception e) {
+			throw new IllegalArgumentException("No se pudo actualizar el orador", e);
+		}
 		
 	}
 
 	
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+		String sql = "delete from orador where id = ?";
+		
+		try(Connection conn = AdministradorDeConexiones.getConnection()) {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setLong(1, id);
+			statement.executeUpdate();
+		}catch(Exception e) {
+			throw new IllegalArgumentException("No se pudo eliminar el orador", e);
+		}
 		
 	}
 
 
 	public List<Orador> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, nombre, apellido, mail, tema, fecha_alta from orador";
+		List<Orador> oradores = new ArrayList<>();
+		
+		try(Connection conn = AdministradorDeConexiones.getConnection()) {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet res = statement.executeQuery();//SELECT
+			//mientras haya oradores extraigo los datos, agrego a la lista
+			while(res.next()) {
+				Long _id = res.getLong(1);
+				String nombre = res.getString(2);
+				String apellido = res.getString(3);
+				String mail = res.getString(4);
+				String tema = res.getString(5);
+				Date fechaAlta = res.getDate(6);
+				
+				Orador orador = new Orador(_id,nombre, apellido, tema, mail, LocalDate.now());/*tph fechaAlta de java.sql.Date a LocalDate*/
+				oradores.add(orador);
+			}
+		}catch (Exception e) {
+			throw new IllegalArgumentException("No se pudo obtener los oradores", e);
+		}
+		return oradores;
 	}
 	
 
