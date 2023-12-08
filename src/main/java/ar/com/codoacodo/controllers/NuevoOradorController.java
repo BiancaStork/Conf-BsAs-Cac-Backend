95390c3a12b26be0081e36259789c06ce89da734
Bianca Stork <biancastrk@gmail.com>
2023-12-08 03:08:11 -0300

    controllers

--- src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
+++ src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
@@ -1,6 +1,7 @@
 package ar.com.codoacodo.controllers;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,8 +21,8 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/api/orador/nuevo")
 
 public class NuevoOradorController extends HttpServlet{
-   
-	//private OradorRepository repository = new MySQLOradorRepository();		
+	
+	private OradorRepository repository = new MySQLOradorRepository();		
 	
 	//enviar por POST todos los datos desde el formulario en el front
 	
@@ -29,40 +30,50 @@ public class NuevoOradorController extends HttpServlet{
 			HttpServletRequest request,//aca viene todos lo del front 
 			HttpServletResponse response)  //aca va hacia el front
 					throws ServletException, IOException {
-		String nombre = request.getParameter("nombre");
-		String apellido = request.getParameter("apellido");
-		String mail = request.getParameter("mail");
-		String tema = request.getParameter("tema");
-		
 		//obtengo el json desde el frontend
-				String json = request.getReader()
-						.lines()
-						.collect(Collectors.joining(System.lineSeparator()));//spring
-				
-				//convierto de json String a Objecto java usando libreria de jackson2
-				ObjectMapper mapper = new ObjectMapper();
-				mapper.registerModule(new JavaTimeModule());
-				mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-				OradorRequest oradorRequest = mapper.readValue(json, OradorRequest.class);
-				
-		//validaciones
-		//if(nombre==null || apellido==null || mail==null || tema==null) {
-			// response json de error
-	//	}
+		String json = request.getReader()
+				.lines()
+				.collect(Collectors.joining(System.lineSeparator()));//spring
 		
-		 OradorRepository repository = new MySQLOradorRepository();
-		 Orador orador = new Orador (oradorRequest.getNombre(), 
-				 oradorRequest.getApellido(), 
-				 oradorRequest.getMail(), 
-				 oradorRequest.getTema(), 
-				 LocalDate.now());
-		 repository.save(orador);
-		 
-		//convierto el Objeto java a String
-		 // envio al frontend atraves del response
-		 response.getWriter().print(mapper.writeValueAsString(orador));		 
+		//convierto de json String a Objecto java usando libreria de jackson2
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		OradorRequest oradorRequest = mapper.readValue(json, OradorRequest.class);
 		
-	}
+		//grabamos en db
 	
+		Orador orador = new Orador(oradorRequest.getNombre(), 
+				oradorRequest.getApellido(), 
+				oradorRequest.getMail(), 
+				oradorRequest.getTema(), 
+				LocalDate.now());
+		
+		repository.save(orador);
+		
+		response.setStatus(HttpServletResponse.SC_CREATED);//201
+		
+		//convierto ahora Objeto java a String
+		//enviar por medio de response al frontend
+		response.getWriter().print(mapper.writeValueAsString(orador));
+	}
 
+	protected void doGet(
+			HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Orador> oradores = this.repository.findAll();
+		
+		//convierto de json String a Objecto java usando libreria de jackson2
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		
+		String jsonParaEnviarAlFrontend = mapper.writeValueAsString(oradores);
+		
+		response.setStatus(HttpServletResponse.SC_OK);
+
+		//escribe la respueta en el objeto response (que despues es lo que recibe el front)
+		response.getWriter().print(jsonParaEnviarAlFrontend );
+	}
 }
